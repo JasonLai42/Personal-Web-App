3b9601d47d76bba17204073e0459265d8674ca31
Jason Lai <jasondoeslai@gmail.com>
2023-04-20 20:36:01 -0700

    V1.0: Added paths to other pages and changed case of root path

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,38 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
-import Root from './routes/partials/Root';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './routes/partials/NavBar';
+import Root from './routes/pages/Root';
+import AboutMe from './routes/pages/AboutMe';
+import Resume from './routes/pages/Resume';
+import ContactMe from './routes/pages/ContactMe';
 
 const router = createBrowserRouter([
   {
     // This is technically our root route, not '/', since GitHub Pages 
     // uses the repo name as the path to the root of the web app.
-    path: '/Personal-Web-App/',
+    path: '/personal-web-app/',
     element: <Root />,
+  },
+  {
+    path: '/personal-web-app/about-me/',
+    element: <AboutMe />,
+  },
+  {
+    path: '/personal-web-app/resume/',
+    element: <Resume />,
+  },
+  {
+    path: '/personal-web-app/contact-me/',
+    element: <ContactMe />,
   }
 ]);
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <RouterProvider router={router} />
     </div>
   );
--- src/routes/partials/NavBar.js
+++ src/routes/partials/NavBar.js
@@ -3,38 +3,20 @@ import React from 'react';
 const NavBar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/Personal-Web-App/">Replace this with image</a>
+            <a class="navbar-brand" href="/personal-web-app/">Replace this with image</a>
 
             <div class="navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/Personal-Web-App/about-me/">About Me</a>
+                    <a class="nav-link" href="/personal-web-app/about-me/">About Me</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/Personal-Web-App/resume/">Resume</a>
+                    <a class="nav-link" href="/personal-web-app/resume/">Resume</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/Personal-Web-App/contact-me/">Contact Me</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link" href="/personal-web-app/contact-me/">Contact Me</a>
                 </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
             </div>
         </nav>
     )

1719ecda59caae703cd7c9ad92229a05c69e898c
Jason Lai <jasondoeslai@gmail.com>
2024-10-26 23:27:32 -0700

    V1.0: add body text to root

--- src/routes/pages/Root.js
+++ src/routes/pages/Root.js
@@ -4,8 +4,14 @@ import fun from './../../assets/personal img/fun.jpg';
 export default function Root() {
     return (
         <>
-            <div id="image-container" class="image-container">
+            <div id="root-image-container" class="root-image-container">
                 <img src={fun} alt="fun image" />
+            </div>
+            <div id="root-text-container" class="root-text-container">
+                <div>Jason Lai</div>
+                <div>Programmer, manga reader, cycling enjoyer, amateur cook, artist</div>
+                <div>Previously @ Rev.com</div>
+                <div>Current M.S. student @ Georgia Tech Computer Science</div>
             </div>
         </>
     );

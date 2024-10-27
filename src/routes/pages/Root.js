96cce2b1cb746b304a41392853f0519de5f9866d
Jason Lai <jasondoeslai@gmail.com>
2024-10-26 23:00:35 -0700

    try this for image

--- src/routes/pages/Root.js
+++ src/routes/pages/Root.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import fun from '../../assets/fun.jpg';
+
 export default function Root() {
     return (
         <>
-            Root!
+            <img src={fun} alt="fun image" />
         </>
     );
 }

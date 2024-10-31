9db4bf6d7af8f2a328dd5848a34669eb678989c1
Jason Lai <jasondoeslai@gmail.com>
2024-10-31 01:23:28 -0700

    V1.0: footer stuff added

--- src/routes/partials/Footer.js
+++ src/routes/partials/Footer.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import linkedin from './../../assets/personal img/linkedin.png';
+import github from './../../assets/personal img/github.png';
+import email from './../../assets/personal img/email.png'
+
 export default function Footer() {
     return (
         <>
-            Footer!
+            <div>
+                <a href="https://www.linkedin.com/in/jasonlai42/" target="_blank">
+                    <img class="social-icon" src={linkedin}></img>
+                </a>
+                <a href="https://github.com/JasonLai42" target="_blank">
+                    <img class="social-icon" src={github}></img>
+                </a>
+                <a href="mailto: jasondoeslai@gmail.com">
+                    <img class="social-icon" src={email}></img>
+                </a>
+            </div>
+            <div class="footer-text">
+                Jason Lai 2024
+            </div>
         </>
     );
 }
